a3fde6a40e4497d29a17809286b45c848b47dc9d
Oksana-Zakhariia <106253728+Oksana-Zakhariia@users.noreply.github.com>
2022-11-13 17:29:38 +0000

    Deploying to gh-pages from @ Oksana-Zakhariia/goit-js-hw-09@91a064507862987f35fa6c72964f82c2a087e754 🚀

---
+++ 01-color-switcher.d07a1025.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.setAttribute("disabled",""),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),e.setAttribute("disabled",""),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.d07a1025.js.map
